b05067e673304c0a7841952c09a90037d6ea14bb
paximoo <odedahmed152@gmail.com>
2024-06-08 15:54:19 +0530

    Created DB and init in app.py

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
+from cs50 import SQL
+from flask_session import Session
+from tempfile import mkdtemp
 
 app = Flask(__name__, template_folder='static//templates')
+
+@app.after_request
+def after_request(response):
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Expires'] = 0
+    response.headers['Pragma'] = 'no-cache'
+    return response
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config['SESSION_FILE_DIR'] = mkdtemp()
+app.config['SESSION_PERMANENT'] = False
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
+
+db = SQL('sqlite:///illuminate.db')
 
 if __name__ == '__main__':
     app.run(port=8080, host='0.0.0.0', debug=True)
